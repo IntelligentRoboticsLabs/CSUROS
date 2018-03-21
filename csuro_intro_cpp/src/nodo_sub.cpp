cc45332028d9aaa22f33bebe7d5c14176847e77f
Francisco Martín Rico <fmrico@gmail.com>
2018-03-21 18:53:05 +0100

    Fix in sub to basic data

--- csuro_intro_cpp/src/nodo_sub.cpp
+++ csuro_intro_cpp/src/nodo_sub.cpp
@@ -1,22 +1,10 @@
 #include "ros/ros.h"
 #include "std_msgs/Int64.h"
-#include "kobuki_msgs/BumperEvent.h"
-#include "geometry_msgs/Twist.h"
 
-geometry_msgs::Twist vel;
 
-void messageCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+void messageCallback(const std_msgs::Int64::ConstPtr& msg)
 {
-  ROS_INFO("Bumper: [%d]", msg->bumper);
-  ROS_INFO("State: [%d]", msg->state);
-
-
-  if(msg->state == 0){
-    vel.linear.x=0;
-  }else{
-    vel.linear.x=0.15;
-  }
-
+  ROS_INFO("Data: [%ld]", msg->data);
 
 }
 
@@ -26,14 +14,12 @@ int main(int argc, char **argv)
    ros::init(argc, argv, "num_subscriber");
    ros::NodeHandle n;
 
-   ros::Subscriber sub = n.subscribe("/mobile_base/events/bumper", 1, messageCallback);
-   ros::Publisher num_pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1);
+   ros::Subscriber sub = n.subscribe("/message", 1, messageCallback);
 
    ros::Rate loop_rate(10);
 
    while (ros::ok())
    {
-     num_pub.publish(vel);
      ros::spinOnce();
      loop_rate.sleep();
    }
